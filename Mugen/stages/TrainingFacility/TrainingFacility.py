72a69b4c56fbd5351157ecc9aa3e16bfcd7133da
sjf60780 <sfaulkner60780@outlook.com>
2018-09-27 19:37:36 -0500

    Sprites added for Goku and Vegeta
    Player can select Goku or Vegeta and the respective sprites are loaded. Supported animation includes standing still, walking left or right, jumping, and falling.

--- Mugen/stages/TrainingFacility/TrainingFacility.py
+++ Mugen/stages/TrainingFacility/TrainingFacility.py
@@ -13,7 +13,6 @@ class TrainingFacility():
         self.platform_list.append(arcade.sprite.Sprite("stages/TrainingFacility/img/platform.jpg", image_width = 190, image_height = 2, center_x = 265, center_y = 325)) # mid left
         self.platform_list.append(arcade.sprite.Sprite("stages/TrainingFacility/img/platform.jpg", image_width = 185, image_height = 2, center_x = 720, center_y = 325)) # mid right
         self.platform_list.append(arcade.sprite.Sprite("stages/TrainingFacility/img/platform.jpg", image_width = 180, image_height = 2, center_x = 490, center_y = 425)) # top
-        self.platform_list.append(arcade.sprite.Sprite("stages/TrainingFacility/img/platform.jpg", image_width = 1000, image_height = 2, center_x = 500, center_y = 0))
 
     def update(self, arcade, game):
         """
@@ -25,8 +24,6 @@ class TrainingFacility():
         """
         Description: This function draws the stage.
         """
-        # Draw Background
         arcade.draw_texture_rectangle(game.gameOptions["window"]["width"] // 2, game.gameOptions["window"]["height"] // 2,
                                       game.gameOptions["window"]["width"], game.gameOptions["window"]["height"], self.background)
-        # Draw platforms (for debugging)
-        # self.platform_list.draw()
+        self.platform_list.draw()
